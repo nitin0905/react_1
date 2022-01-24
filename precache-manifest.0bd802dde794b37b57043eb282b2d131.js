fa475891889c5b6b2b40279b593ad6e427ec9b4f
nitin0905 <nk65531@gmail.com>
2022-01-24 14:44:48 +0530

    Updates

---
+++ precache-manifest.0bd802dde794b37b57043eb282b2d131.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74442e67b91fd1dfd712aaf07fda45f",
+    "url": "/react_1/index.html"
+  },
+  {
+    "revision": "279dccd539a60fecbe66",
+    "url": "/react_1/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "e86ad4ae70e9abb6f97e",
+    "url": "/react_1/static/css/main.fde1ca09.chunk.css"
+  },
+  {
+    "revision": "279dccd539a60fecbe66",
+    "url": "/react_1/static/js/2.d1e43b3c.chunk.js"
+  },
+  {
+    "revision": "e86ad4ae70e9abb6f97e",
+    "url": "/react_1/static/js/main.71d5d346.chunk.js"
+  },
+  {
+    "revision": "2dd872a0ba06f77ddf19",
+    "url": "/react_1/static/js/runtime~main.3515f5ea.js"
+  },
+  {
+    "revision": "305ed564fd1124c284d7d421f46ce165",
+    "url": "/react_1/static/media/1.305ed564.png"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/react_1/static/media/2.619f0cc8.png"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/react_1/static/media/3.1192a86d.jpg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/react_1/static/media/4.70cea92a.jpeg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/react_1/static/media/5.43049cae.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/react_1/static/media/6.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/react_1/static/media/7.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/react_1/static/media/8.2eed95a2.jpg"
+  }
+]);
